46cdb9b466e202cce25967969632a5d664ff0543
OM <126644998+OmSinha07@users.noreply.github.com>
2024-01-22 23:57:19 +0530

    Add files via upload
    20.1.24 javalab

---
+++ javalabprg/Firstprg.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Firstprg
+{
+	public static void main(String args[])
+	{
+Scanner in=new Scanner(System.in);
+String name;
+System.out.println("Hello world");
+name=in.nextLine();
+System.out.println("hello "+name);
+}
+}
---
+++ javalabprg/varribletypes.java
@@ -0,0 +1,13 @@
+public class VarriableTypes
+{
+int instancevarriables;
+String instancevarriables= "Instance Varriable";
+
+Static int staticVar1;
+Static String staticVar1="Static Varriable";
+public static void main(String args[])
+{
+int local varriable;
+String localVar2 = "Local Varriable";
+VarriableTypes vt =new VarriableTypes();
+System.out.println("
